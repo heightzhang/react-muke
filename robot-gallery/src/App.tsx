c88c97676d1006153e81540a4708d1f4390f32a2
zhanggao <zhanggao@iboxpay.com>
2022-02-24 20:17:48 +0800

    feat: 3-12 事件Event驱动

--- robot-gallery/src/App.tsx
+++ robot-gallery/src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './assets/images/logo.svg'
 // import './App.css';
 import robots from "./mockdata/robots.json";
 import Robot from "./components/Robot";
 import styles from "./App.module.css";
+import ShoppingCart from './components/ShoppingCart'
 
 
 function App() {
   return (
     <div className={styles.app}>
+      {/*  */}
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo"></img>
+        <h1>标题要长</h1>
+      </div>
+
+      {/*  */}
+      <ShoppingCart></ShoppingCart>
+
+      {/*  */}
       <div className={styles.robotList}>
         <div className={styles.robotList}>
           {robots.map((r) => (
