8ac175a0d4db8046ba051d7a295fc1bc2c82275e
heightzhang <heightzhang@163.com>
2022-03-01 20:16:03 +0800

    feat: react-travel init

---
+++ react-travel/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import styles from './App.modules.css';
+console.log('styles', styles)
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <header className={styles['App-header']}>
+        <img src={logo} className={styles['App-logo']} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className={styles['App-link']}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
