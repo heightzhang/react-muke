1ad76125b563f57f4e22f0edcfd170c19dbe35c7
heightzhang <heightzhang@163.com>
2022-03-11 19:53:40 +0800

    feat: 增加语言翻译

--- react-travel/src/pages/home/HomePage.tsx
+++ react-travel/src/pages/home/HomePage.tsx
@@ -6,11 +6,15 @@ import { productList1, productList2, productList3 } from './mockups';
 import sideImage from "../../assets/images/sider_2019_12-09.png";
 import sideImage2 from "../../assets/images/sider_2019_02-04.png";
 import sideImage3 from "../../assets/images/sider_2019_02-04-2.png";
+import { withTranslation, WithTranslation } from "react-i18next";
 
 
 
-export class HomePage extends React.Component {
+class HomePageComponent extends React.Component<WithTranslation> {
   render() {
+    const { t } = this.props
+    console.log('t', t('home_page.hot_recommended'))
+
     return (
       <div>
         <Header />
@@ -26,7 +30,7 @@ export class HomePage extends React.Component {
           <ProductCollection
             title={
               <Typography.Title level={3} type='warning'>
-                爆款推荐
+                {t('home_page.hot_recommended')}
               </Typography.Title>}
             sideImage={sideImage}
             products={productList1}
@@ -35,7 +39,7 @@ export class HomePage extends React.Component {
           <ProductCollection
             title={
               <Typography.Title level={3} type="danger">
-                新品上市
+                {t("home_page.new_arrival")}
               </Typography.Title>
             }
             sideImage={sideImage2}
@@ -46,7 +50,7 @@ export class HomePage extends React.Component {
           <ProductCollection
             title={
               <Typography.Title level={3} type="success">
-                国内游推荐
+                {t("home_page.domestic_travel")}
               </Typography.Title>
             }
             sideImage={sideImage3}
@@ -61,3 +65,5 @@ export class HomePage extends React.Component {
     )
   }
 }
+
+export const HomePage = withTranslation()(HomePageComponent)
