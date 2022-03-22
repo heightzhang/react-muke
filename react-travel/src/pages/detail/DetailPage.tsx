4c06132d292c2fc4fb4864c4a46ed482aee578b8
heightzhang <heightzhang@163.com>
2022-03-22 22:02:58 +0800

    feat:  TypeScript init

--- react-travel/src/pages/detail/DetailPage.tsx
+++ react-travel/src/pages/detail/DetailPage.tsx
@@ -4,7 +4,7 @@ import { RouteComponentProps } from 'react-router-dom'
 interface MatchParams {
   touristRouteId: string
 }
-
+ 
 export const DetailPage: React.FC<RouteComponentProps<MatchParams>> = (props) => {
   return <h1>路线ID：{props.match.params.touristRouteId} </h1>
 }
