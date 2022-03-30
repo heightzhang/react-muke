0bcb32d289781059a3b03b581a09fcd835e325c0
zhanggao <zhanggao@iboxpay.com>
2022-03-30 21:17:09 +0800

    feat: 请求列表详情数据

--- react-travel/src/index.tsx
+++ react-travel/src/index.tsx
@@ -7,6 +7,9 @@ import "antd/dist/antd.css";
 import './i18n/configs';
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import axios from "axios";
+
+axios.defaults.headers = Object.assign(axios.defaults.headers, {'x-icode': '10F159C7B862011C'})
 
 ReactDOM.render(
   <React.StrictMode>
--- react-travel/src/pages/detail/DetailPage.tsx
+++ react-travel/src/pages/detail/DetailPage.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import { RouteComponentProps } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { RouteComponentProps, useParams } from 'react-router-dom'
+import axios from 'axios'
+
 
 interface MatchParams {
   touristRouteId: string
 }
- 
+
 export const DetailPage: React.FC<RouteComponentProps<MatchParams>> = (props) => {
+  const { touristRouteId } = useParams<MatchParams>()
+  const [ loading, setLoading ] = useState<boolean>()
+  const [ product, setProduct ] = useState<any>()
+  const [ error, setError ] = useState<string | null>()
+
+  // const testId = 'fb6d4f10-79ed-4aff-a915-4ce29dc9c7e1'
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      try {
+        const { data } = await axios.get(
+          `http://123.56.149.216:8080/api/touristRoutes/fb6d4f10-79ed-4aff-a915-4ce29dc9c7e1`
+          // `http://123.56.149.216:8080/api/touristRoutes/${testId}`
+        )
+        setProduct(data)
+        setLoading(false)
+      } catch (error: any) {
+        setError(error.message)
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [touristRouteId])
+
+  console.log({loading, product, error})
+
   return <h1>路线ID：{props.match.params.touristRouteId} </h1>
 }
